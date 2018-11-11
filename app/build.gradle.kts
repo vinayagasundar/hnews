5a4de6093f2c4710f610d9a034b7ba7d518260db
Vinayagasundar S <vinayagasundar93@gmail.com>
2018-11-11 23:22:26 +0800

    (feat) Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,52 @@
+android {
+    defaultConfig {
+        applicationId = "com.blackcatz.android.hnews"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(AppConfig.Libs.Kotlin.jvm)
+    implementation(AppConfig.Libs.Kotlin.coroutineAndroid)
+    implementation(AppConfig.Libs.Support.appCompat)
+    implementation(AppConfig.Libs.Support.constraitLayout)
+
+    implementation(AppConfig.Libs.Support.materialDesign)
+    implementation(AppConfig.Libs.Support.recyclerView)
+
+
+    implementation(AppConfig.Libs.Support.lifeCycle)
+    implementation(AppConfig.Libs.Support.room)
+
+    implementation(AppConfig.Libs.BlackCatz.daggerbase)
+
+    implementation(AppConfig.Libs.Dagger.daggerAndroid)
+    implementation(AppConfig.Libs.Dagger.daggerAndroidSupport)
+
+    implementation(AppConfig.Libs.Network.retrofit2)
+    implementation(AppConfig.Libs.Network.okhttpLogger)
+    implementation(AppConfig.Libs.Network.gsonConv)
+    implementation(AppConfig.Libs.Image.picasso)
+
+    debugImplementation(AppConfig.Libs.Debug.dbDebug)
+
+
+    kapt(AppConfig.Libs.Dagger.daggerCompiler)
+    kapt(AppConfig.Libs.Dagger.daggerAndroidCompiler)
+    kapt(AppConfig.Libs.Support.roomCompiler)
+
+    testImplementation(AppConfig.Libs.Test.junit)
+    testImplementation(AppConfig.Libs.Support.roomTestHelper)
+    testImplementation(AppConfig.Libs.Test.Mockito.nhaarmanMock)
+    testImplementation(AppConfig.Libs.Network.retrofit2Mock)
+
+    androidTestImplementation(AppConfig.Libs.AndroidTest.testRunner)
+    androidTestImplementation(AppConfig.Libs.AndroidTest.espressoCore)
+}
---
+++ build.gradle.kts
@@ -0,0 +1,93 @@
+import com.android.build.gradle.BaseExtension
+import com.android.builder.model.Dependencies
+import org.jetbrains.kotlin.gradle.dsl.Coroutines
+import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath(AppConfig.Plugins.android)
+        classpath(AppConfig.Plugins.kotlin)
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven {
+            setUrl("https://dl.bintray.com/vinayagasundar/BlackCatz/")
+        }
+    }
+
+    if ((group as String).isNotEmpty()) {
+        if (name == "app")
+            configureAndroid()
+
+        if (name == "android-client")
+            configureAndroidLibs()
+
+        if (name == "model" || name == "api")
+            configureKotlin()
+    }
+}
+
+
+fun Project.configureKotlin() {
+    apply(plugin = "kotlin")
+
+    configure<KotlinProjectExtension> {
+        experimental.coroutines = Coroutines.ENABLE
+    }
+}
+
+
+fun Project.configureAndroid() {
+    apply(plugin = "com.android.application")
+    apply(plugin = "kotlin-android")
+    apply(plugin = "kotlin-android-extensions")
+    apply(plugin = "kotlin-kapt")
+
+    configure<BaseExtension> {
+        compileSdkVersion(AppConfig.SdkVersion.compile)
+
+        defaultConfig {
+            minSdkVersion(AppConfig.SdkVersion.min)
+            targetSdkVersion(AppConfig.SdkVersion.target)
+            versionCode = 1
+            versionName = "1.0"
+        }
+    }
+
+    configure<KotlinProjectExtension> {
+        experimental.coroutines = Coroutines.ENABLE
+    }
+}
+
+fun Project.configureAndroidLibs() {
+    apply(plugin = "com.android.library")
+    apply(plugin = "kotlin-android")
+    apply(plugin = "kotlin-android-extensions")
+    apply(plugin = "kotlin-kapt")
+
+    configure<BaseExtension> {
+        compileSdkVersion(AppConfig.SdkVersion.compile)
+
+        defaultConfig {
+            minSdkVersion(AppConfig.SdkVersion.min)
+            targetSdkVersion(AppConfig.SdkVersion.target)
+        }
+    }
+
+    configure<KotlinProjectExtension> {
+        experimental.coroutines = Coroutines.ENABLE
+    }
+}
